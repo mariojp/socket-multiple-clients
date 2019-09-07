8dc910aa8c96bf4f2c19541cf89e7ad70998f4d2
Mario Jorge Pereira <mariojp@gmail.com>
2019-09-07 13:55:05 -0300

    Initial commit

---
+++ project/src/test/java/br/com/mariojp/project/AppTest.java
@@ -0,0 +1,21 @@
+package br.com.mariojp.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+   
+
+  
+
+    @Test
+    public void testApp(){
+    	App.main(null);
+    	assertTrue(true);
+    }
+}
